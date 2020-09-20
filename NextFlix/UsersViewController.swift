edf6abff84b0ac36caebfb5c12f95f9dd1572fb4
William Daugherty <raidantlabs@gmail.com>
2020-09-19 23:35:54 -0700

    Early NextFlix

---
+++ NextFlix/UsersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UsersViewController.swift
+//  NextFlix
+//
+//  Created by William Daugherty on 9/19/20.
+//  Copyright © 2020 William Daugherty. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("Hello")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
