95f110412b0542dbd756988f853829573a2ad9db
William Daugherty <raidantlabs@gmail.com>
2020-09-26 19:00:15 -0700

    Added Movie Details

---
+++ NextFlix/MovieDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieDetailsViewController.swift
+//  NextFlix
+//
+//  Created by William Daugherty on 9/26/20.
+//  Copyright © 2020 William Daugherty. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    var movie: [String:Any]!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print(movie["title"]!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
