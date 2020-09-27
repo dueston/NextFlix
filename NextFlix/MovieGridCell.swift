3aa4a4b3e0195e3f87da8f1658b53aecbe704ae9
William Daugherty <raidantlabs@gmail.com>
2020-09-27 03:28:04 -0700

    Buggy but progress

---
+++ NextFlix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  NextFlix
+//
+//  Created by William Daugherty on 9/27/20.
+//  Copyright © 2020 William Daugherty. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
